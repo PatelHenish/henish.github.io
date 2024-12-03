32e6418c9effd57a503c86473d27bf78d6d919c1
PatelHenish <7015287@spsd.sk.ca>
2024-12-03 11:27:14 -0600

    STARTED YESTURDAY BUT FORGET TO COMMENT  TILL HERE

---
+++ Capstone prject/sketch.js
@@ -0,0 +1,14 @@
+// Capstonbe project 
+// Henish and Sahil "patel" 
+// 12/02/2024    Month/day/year
+// We are creating a simple and working chess game  
+
+const pieceSymbol = {
+  empty:"",
+  whitePawn: "",
+  WhiteRook: "",
+  WhiteKnight: "",
+  WhiteBishop: "",  
+}
+
+
